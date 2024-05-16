561cc22dcc18db0226f94940f572c5993963fd94
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2024-05-16 19:01:40 +0000

    Organize options, flags, and files into arrays

--- main.c
+++ main.c
@@ -9,17 +9,57 @@ int main(int argc, char** argv) {
 		help();
 		return 0;
 	}
+	int opt_count = 0, flg_count = 0, file_count = 0;
 	for (int i=1;i<argc;i++) {
 		char* arg = argv[i];
 		if (arg[0] == '-' && arg[1] == '-') {
 			// Option
+			opt_count++;
 		} else if (arg[0] == '-') {
 			// Flags
+			for (int j=1;j<strlen(arg);j++) {
+				flg_count++;
+			}
 		} else {
 			// File
+			file_count++;
 		}
 	}
 
+	// DBG
+	//printf("%d\n", opt_count);
+	//printf("%d\n", flg_count);
+	//printf("%d\n", file_count);
+	// !DBG
+	char* options[opt_count];
+	char flags[flg_count];
+	char* filenames[file_count];
+	opt_count--;
+	flg_count--;
+	file_count--;
+
+	for (int i=1;i<argc;i++) {
+		char* arg = argv[i];
+		if (arg[0] == '-' && arg[1] == '-') {
+			// Option
+			//printf("Option: %s\n", arg);	//DBG
+			options[opt_count] = arg;
+			opt_count--;
+		} else if (arg[0] == '-') {
+			// Flags
+			for (int j=1;j<strlen(arg);j++) {
+				//printf("Flag: %c\n", arg[j]);	//DBBG
+				char c = arg[j];
+				flags[flg_count] = c;
+				flg_count--;
+			}
+		} else {
+			// File
+			//printf("File: %s\n", arg);	//DBG
+			filenames[file_count] = arg;
+			file_count--;
+		}
+	}
 	return 0;
 }
 
